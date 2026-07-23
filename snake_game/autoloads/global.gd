611d4221587918d3a90e739a3e3cd286e7353f12
vii-abdullatif <dows5750@gmail.com>
2026-07-23 16:48:58 +0300

    Initial commit: Added project root structure and Snake sub-game setup & Player input

---
+++ snake_game/autoloads/global.gd
@@ -0,0 +1,3 @@
+extends Node
+
+const grid_size : int = 32
---
+++ snake_game/gameplay/game_play.gd
@@ -0,0 +1,38 @@
+class_name GamePlay
+extends Node2D
+
+@onready var head: Head = %Head as Head
+
+var time_between_moves : float = 1000.0
+var time_since_last_move : float = 0
+var speed : float = 1000.0
+var move_dir: Vector2 = Vector2.RIGHT
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("ui_up"):
+		move_dir = Vector2.UP
+	elif Input.is_action_pressed("ui_down"):
+		move_dir = Vector2.DOWN
+	elif Input.is_action_pressed("ui_right"):
+		move_dir = Vector2.RIGHT
+	elif Input.is_action_pressed("ui_left"):
+		move_dir = Vector2.LEFT
+
+func _physics_process(delta: float) -> void:
+	time_since_last_move += delta * speed
+	if time_since_last_move >= time_between_moves:
+		update_snake()
+		time_since_last_move = 0
+
+func update_snake():
+	print("move the snake")
+	var new_pos : Vector2 = head.position + move_dir * Global.grid_size
+	head.move_to(new_pos)
+	pass
+	
